a8be6bd3b893dade94524b71a78cd111911992df
cniscoding <asdfa.com>
2023-02-16 19:38:17 -0800

    working index.js

--- fetchBreedDescription.js
+++ fetchBreedDescription.js
@@ -1,5 +1,6 @@
 
 const request = require('request');
+
 const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     const data = JSON.parse(body);
@@ -14,7 +15,7 @@ const fetchBreedDescription = function(breedName, callback) {
 };
 
 
-module.exports(fetchBreedDescription);
+module.exports = fetchBreedDescription;
 
 
 
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const {fetchBreedDescription} = require('./fetchBreedDescription');
+const fetchBreedDescription = require('./fetchBreedDescription');
 
 const breedName = process.argv[2];
 
 fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
+    
   } else {
     console.log(desc);
   }
