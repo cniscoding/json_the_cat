8d1f2f52eab841cdd19abbaeff59b2a4bfdd60f5
cniscoding <asdfa.com>
2023-02-16 17:48:37 -0800

    working search and edge cases added. eslint completed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,7 +1,17 @@
-const request = require('request')
+const request = require('request');
+const args = process.argv.slice(2);
 
-
-
-request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (error, response, body) => {
-  console.log('body:', body); // Print the HTML for the Google homepage.
+request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
+  // console.log('body:', body); // Print the HTML for the Google homepage.
+  const data = JSON.parse(body);
+  if (response.statusCode !== 200) {
+    console.log(`The Website was ${response.statusMessage}`);
+    return;
+  }
+  if (data[0] === undefined) {
+    console.log('breed not found');
+    return;
+  }
+  console.log(data[0].description);
 });
+
