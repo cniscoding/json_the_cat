bf5ac10a710c66117738b7b38be0290e4b122612
cniscoding <asdfa.com>
2023-02-16 18:59:15 -0800

    updated edge case

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,14 +4,11 @@ const args = process.argv.slice(2);
 request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
   // console.log('body:', body); // Print the HTML for the Google homepage.
   const data = JSON.parse(body);
-  if (response.statusCode !== 200) {
-    console.log(`The Website was ${response.statusMessage}`);
-    return;
-  }
   if (data[0] === undefined) {
     console.log('breed not found');
     return;
   }
   console.log(data[0].description);
 });
+
 
