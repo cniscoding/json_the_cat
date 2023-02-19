d0bec2ea98159e07680b11a3e9a79658d9291c8d
cniscoding <asdfa.com>
2023-02-18 21:31:55 -0800

    working fetchBreedDescription

--- fetchBreedDescription.js
+++ fetchBreedDescription.js
@@ -5,19 +5,19 @@ const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     const data = JSON.parse(body);
     if (data[0] === undefined) {
-      error = 'breed not found';
+      error;
       
     }
     if (error) {
-      callback(error);
+      callback(error, null);
       return;
     }
-      callback(null, data[0].description);
+    callback(null, data[0].description);
   });
 };
 
 
-module.exports = fetchBreedDescription;
+module.exports = {fetchBreedDescription};
 
 
 
